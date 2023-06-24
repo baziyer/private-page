079dab48803f10a30227e5bcfe89beefff34b0e1
baziyer <109233175+baziyer@users.noreply.github.com>
2023-06-24 16:40:37 +0100

    Updoot
    Regular

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
+      <h1 className="title">
+  Read <Link href="/posts/firstpost">this page!</Link>
+</h1>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
